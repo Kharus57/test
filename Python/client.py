139593f695754b3089902e2fada10cf7baa30297
Ruslan <funsci@mail.ru>
2018-03-19 20:43:20 +0300

    add python2

---
+++ Python/client.py
@@ -0,0 +1,33 @@
+# Клиент
+
+from socket import *
+import datetime
+import json
+
+
+def create_request_presence(timestamp=None):
+    """
+    Сообщение о присутствии пользователя
+    >>> create_request_presence("2018-03-16 19:03:54.438645")
+    '{"action": "presence", "time": "2018-03-16 19:03:54.438645"}'
+    >>>
+    :param timestamp: Метка времени
+    :return:
+    """
+    if not timestamp:
+        timestamp = str(datetime.datetime.now())
+    result = {
+        "action": "presence",
+        "time": timestamp
+    }
+    return json.dumps(result)
+
+
+if __name__=="__main__":
+    s = socket()
+    s.connect(('localhost', 8882))
+    request_presence = create_request_presence()
+    s.send(request_presence.encode("utf-8"))
+    server_response = s.recv(1024)
+    print(server_response.decode("utf-8"))
+    s.close()
---
+++ Python/server.py
@@ -0,0 +1,38 @@
+# Сервер
+
+from socket import *
+import json
+
+
+def response_200(message):
+    """
+    Ответ с кодом 200
+    >>> response_200("Hello!")
+    '{"response": 200, "alert": "Hello!"}'
+    >>>
+    :param message: необязательное сообщение
+    :return:
+    """
+    result = {
+        "response": 200,
+        "alert": message
+    }
+    return json.dumps(result)
+
+
+if __name__=="__main__":
+    s = socket()
+    s.bind(('', 8882))
+    s.listen(5)
+
+    while True:
+        client, addr = s.accept()
+        print("Получен запрос на соединение от %s" % str(addr))
+        client_request = client.recv(1024)
+        print("Получено сообщение: " + client_request.decode("utf-8"))
+        request = json.loads(client_request.decode("utf-8"))
+        if "action" in request:
+            if request["action"] == "presence":
+                response = response_200("Nice to meet")
+                client.send(response.encode("utf-8"))
+        client.close()
